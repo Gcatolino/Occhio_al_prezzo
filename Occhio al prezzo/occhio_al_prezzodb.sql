d20b44caf7793dc1c85310eba3c387fa8942ee0c
andreapilato <andreapilato@192.168.1.5>
2015-12-10 16:28:44 +0100

    Modifica DB e popolamento del suddetto.

--- Occhio al prezzo/occhio_al_prezzodb.sql
+++ Occhio al prezzo/occhio_al_prezzodb.sql
@@ -6,8 +6,8 @@
 # https://github.com/sequelpro/sequelpro
 #
 # Host: 127.0.0.1 (MySQL 5.7.10)
-# Database: OcchioAlPrezzo
-# Generation Time: 2015-12-08 13:49:53 +0000
+# Database: sys
+# Generation Time: 2015-12-10 14:41:13 +0000
 # ************************************************************
 
 
@@ -30,9 +30,9 @@ CREATE TABLE `Account` (
   `password` varchar(25) NOT NULL,
   `nome` varchar(25) DEFAULT NULL,
   `cognome` varchar(25) DEFAULT NULL,
-  `data di nascita` date DEFAULT NULL,
+  `data_di_nascita` date DEFAULT NULL,
   `domicilio` varchar(40) DEFAULT NULL,
-  `comune di residenza` varchar(40) DEFAULT NULL,
+  `comune_di_residenza` varchar(40) DEFAULT NULL,
   `ruolo` varchar(15) NOT NULL DEFAULT 'utente',
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=hebrew;
@@ -40,11 +40,13 @@ CREATE TABLE `Account` (
 LOCK TABLES `Account` WRITE;
 /*!40000 ALTER TABLE `Account` DISABLE KEYS */;
 
-INSERT INTO `Account` (`email`, `password`, `nome`, `cognome`, `data di nascita`, `domicilio`, `comune di residenza`, `ruolo`)
+INSERT INTO `Account` (`email`, `password`, `nome`, `cognome`, `data_di_nascita`, `domicilio`, `comune_di_residenza`, `ruolo`)
 VALUES
 	('gemma@hotmail.it','miaomiao','gemma','catolino','1991-01-31','via gaudo','baranello','utente'),
-	('miao@gmail.com','baobao','miao','bao','1990-02-23','via ciao','campobasso','venditore');
-
+	('miao@gmail.com','baobao','miao','bao','1990-02-23','via ciao','campobasso','venditore'),
+        ('a@gmail.com','baobao','miao','bao','1980-02-14','piazza garibaldi','salerno','venditore'),
+        ('ven@gmail.com','baobao','miao','bao','1970-02-23','via mario palermo','napoli','venditore'),
+        ('sisa@gmail.com','baobao','miao','bao','1970-02-23','via mario palermo','napoli','venditore');
 /*!40000 ALTER TABLE `Account` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,11 +82,11 @@ DROP TABLE IF EXISTS `Composizione`;
 
 CREATE TABLE `Composizione` (
   `fk_idCarrello` varchar(25) NOT NULL,
-  `fk_idProdotto` varchar(15) NOT NULL,
+  `fk_idProdotto` int(15) NOT NULL,
   PRIMARY KEY (`fk_idCarrello`,`fk_idProdotto`),
   KEY `fk_idProdotto` (`fk_idProdotto`),
   CONSTRAINT `composizione_ibfk_1` FOREIGN KEY (`fk_idCarrello`) REFERENCES `Carrello` (`idCarrello`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `composizione_ibfk_2` FOREIGN KEY (`fk_idProdotto`) REFERENCES `prodotto` (`idProdotto`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `composizione_ifbk_2` FOREIGN KEY (`fk_idProdotto`) REFERENCES `Prodotto` (`idProdotto`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=hebrew;
 
 LOCK TABLES `Composizione` WRITE;
@@ -92,7 +94,7 @@ LOCK TABLES `Composizione` WRITE;
 
 INSERT INTO `Composizione` (`fk_idCarrello`, `fk_idProdotto`)
 VALUES
-	('1','2');
+	('1',12);
 
 /*!40000 ALTER TABLE `Composizione` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -104,7 +106,7 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `Prodotto`;
 
 CREATE TABLE `Prodotto` (
-  `idProdotto` varchar(15) NOT NULL,
+  `idProdotto` int(15) NOT NULL AUTO_INCREMENT,
   `marca` varchar(15) NOT NULL DEFAULT '',
   `nome` varchar(20) NOT NULL DEFAULT '',
   `taglia` varchar(10) NOT NULL DEFAULT '',
@@ -114,15 +116,29 @@ CREATE TABLE `Prodotto` (
   `data` date NOT NULL,
   PRIMARY KEY (`idProdotto`),
   KEY `fk_email` (`fk_email`),
-  CONSTRAINT `prodotto_ibfk_1` FOREIGN KEY (`fk_email`) REFERENCES `account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=hebrew;
+  CONSTRAINT `prodotto_ibfk_1` FOREIGN KEY (`fk_email`) REFERENCES `Account` (`email`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=hebrew;
 
 LOCK TABLES `Prodotto` WRITE;
 /*!40000 ALTER TABLE `Prodotto` DISABLE KEYS */;
 
-INSERT INTO `Prodotto` (`idProdotto`, `marca`, `nome`, `taglia`, `prezzo`, `punto vendita`, `fk_email`, `data`)
+INSERT INTO `Prodotto` (`idProdotto`, `marca`, `nome`, `taglia`, `prezzo`, `punto_vendita`, `fk_email`, `data`)
 VALUES
-	('2','forst','birra','300','2,50','conad','miao@gmail.com','2010-01-31');
+	(1,'Heineken','Birra','330','1.2','Carrefour','miao@gmail.com','2012-02-01'),
+	(2,'San Carlo','Patatine','100','1','Carrefour','miao@gmail.com','2011-05-06'),
+	(3,'Mulino bianco','abbracci','500','2.75','Carrefour','miao@gmail.com','2012-12-02'),
+	(4,'Pavesi','pavesini','375','2.2','Deco','a@gmail.com','2011-02-01'),
+	(5,'San Carlo','Patatine','300','1.1','Deco','a@gmail.com','2011-02-01'),
+	(6,'Asiago','formaggio','1','14.9','Deco','a@gmail.com','2010-01-31'),
+	(7,'Nastro Azzurro','Birra','330','0.7','Conad','ven@gmail.com','2012-02-01'),
+	(8,'Lavazza','caffe','500','3.49','Conad','ven@gmail.com','2014-02-01'),
+	(9,'Parmalat','latte','1','0.69','Conad','ven@gmail.com','2015-02-01'),
+	(10,'Heineken','Birra','330','1.0','Sisa','sisa@gmail.com','2011-02-01'),
+	(11,'Carapelli','Olio di oliva','1','2.99','Sisa','sisa@gmail.com','2011-02-01'),
+	(12,'Ferrero','nutella','625','4.29','Sisa','sisa@gmail.com','2015-02-01'),
+	(13,'Granarolo','latte','1','1.99','Sisa','sisa@gmail.com','2011-02-01'),
+	(14,'Dash','detersivo liquido','50','6.89','Sisa','sisa@gmail.com','2013-02-01'),
+	(15,'Bauli','Pandoro','1','3.99','Sisa','sisa@gmail.com','2015-12-09');
 
 /*!40000 ALTER TABLE `Prodotto` ENABLE KEYS */;
 UNLOCK TABLES;
