fc866727eec0e9fccb760312f2d99f7b5ee3c81e
raffaele <raffaele@DESKTOP-IKTDOCA>
2015-12-10 10:22:50 +0100

    Ho aggiunto l'import giusto per utilizzare la classe caricata da Andrea ovvero:DBConnection

--- Occhio al prezzo/src/java/it/unisa/account/AccountManager.java
+++ Occhio al prezzo/src/java/it/unisa/account/AccountManager.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import it.unisa.product.DBConnection;
+import it.unisa.utility.DBConnection;
 import java.util.ArrayList;
 
 /**
